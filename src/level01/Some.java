7f3cf84545927acad726ad08af8f21b5227889ab
Lsapee <dlworhks94@gmail.com>
2021-12-03 19:05:00 +0900

    12/03

---
+++ src/level01/Some.java
@@ -0,0 +1,15 @@
+package level01;
+
+public class Some {
+
+	public static void main(String[] args) {
+		
+		float sum = 100.0f;
+
+		for (int i=0; i<=261; i++) {
+			sum = sum*1.03f;
+		}
+		System.out.println(sum);
+	}
+
+}
