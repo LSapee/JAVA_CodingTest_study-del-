e66c96fd6f6fdf1097dedf4eb4f97c8dd69dd8c3
Lsapee <dlworhks94@gmail.com>
2022-01-19 17:43:28 +0900

    level 01 -1

--- src/level01/LostCloth.java
+++ src/level01/LostCloth.java
@@ -3,44 +3,53 @@ package level01;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 public class LostCloth {
 
 	public static void main(String[] args) {
-		int n = 5;
-		int[] lost = {2,4};
-		int[] reserve = {1,3,5};
+		int n = 10;
+		int[] lost = {1,2,3,5,6};
+		int[] reserve = {2,3,4,5};
 		int answer = n-lost.length;
-		
-		
-		
-		for(int i =0; i<reserve.length; i++) {
-		if(answer >= n) {
-			answer = n;
-			break;
+
+		Arrays.sort(lost);
+		Arrays.sort(reserve);
+
+		ArrayList<Integer> al = new ArrayList<>();
+		ArrayList<Integer> al2 = new ArrayList<>();
+
+		for(int i = 0; i<lost.length; i++){
+			al.add(lost[i]);
 		}
-		for(int j =0; j<lost.length; j++) {
-			if(reserve[i] == lost[j]) {
-				answer +=1;
-				break;
-			}else if(reserve[i] == lost[j]-1 ||reserve[i] == lost[j]+1) {
-				answer +=1;
-				break;
+		for(int i =0; i<reserve.length; i++){
+			al2.add(reserve[i]);
+		}
+		for(int i =0; i<al.size(); i++) {
+			for (int j = 0; j < al2.size(); j++) {
+				if(al.get(i)==al2.get(j)){
+					al.remove(i);
+					al2.remove(j);
+					answer +=1;
+					i=i-1;
+				}
 			}
 		}
-	}
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		System.out.println(al);
+		System.out.println(al2);
+
+		for(int i =0; i<al.size(); i++){
+			for(int j=0; j<al2.size(); j++){
+				if(al.get(i)+1  == al2.get(j) || al.get(i)-1  == al2.get(j)){
+					answer +=1;
+					al.remove(i);
+					break;
+				}
+			}
+		}
+
+		System.out.println(answer);
+
 //		ArrayList<Integer> all = new ArrayList<>(); 
 //		
 //		Arrays.sort(lost);
