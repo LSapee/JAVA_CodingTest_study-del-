e41ab652a5096c356f9e506ecf5335544b42fab5
Lsapee <dlworhks94@gmail.com>
2022-01-27 23:12:48 +0900

    0127 3일차.. 내일 못풀면 다른거 먼저

--- src/level01/Kakao2022.java
+++ src/level01/Kakao2022.java
@@ -2,49 +2,23 @@ package level01;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.HashMap;
 
 public class Kakao2022 {
     public static void main(String[] args){
         String[] id_list = {"muzi", "frodo", "apeach", "neo"};
         String[] report = {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"};
         int k =2;
-        int count = 0;
         int[] answer = new int[id_list.length];
 
-        //        중복제거 report
-        Arrays.sort(report);
-        ArrayList<String> reportMember = new ArrayList<>(Arrays.asList(report));
-        ArrayList<String> stopMember = new ArrayList<>(Arrays.asList(report));
+        HashMap reportMap = new HashMap();
 
-        for(int i =0; i< reportMember.size()-1; i++){
-            if(reportMember.get(i).equals(reportMember.get(i+1))){
-                reportMember.remove(i);
-            }
+        for(int i =0; i< report.length; i++){
+            String[] a = report[i].split(" ");
+            reportMap.put(a[0],a[1]);
         }
-        System.out.println(reportMember);
-//      정지당한 아이디 구하기
-        for(int i =0; i<id_list.length; i++){
-            for(int j = 0; j< reportMember.size(); j++){
-                if(reportMember.get(j).contains(" "+id_list[i])){
-                   reportMember.set(j,reportMember.get(j).replace(id_list[i]+" ",""));
-                }
-            }
-        }
-        System.out.println(reportMember);
-        Collections.sort(reportMember);
-            for(int j = 0; j< reportMember.size()-1; j++){
-                if(reportMember.get(j).equals(reportMember.get(j+1))){
-                    count++;
-                }
-                if(count == k){
-                    stopMember.add(reportMember.get(j));
-                    count =0;
-                }
-            }
+        System.out.println(reportMap.get("muzi"));
+        reportMap.
 
-        System.out.println(stopMember);
-
-        System.out.println(Arrays.toString(answer));
     }
 }
