d8cc1aff06ef96a909e3cb628e4035f04d9dc2a3
Lsapee <dlworhks94@gmail.com>
2022-02-14 15:19:18 +0900

    브론즈 문제

---
+++ src/bronze/NameA.java
@@ -0,0 +1,13 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class NameA {
+    public static void main(String aa[]){
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next() + "??!";
+
+        System.out.println(a);
+    }
+}
---
+++ src/bronze/YearBu.java
@@ -0,0 +1,13 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class YearBu {
+    public static void main(String[] aa){
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt()-553;
+
+        System.out.println(a);
+    }
+}
