39d6e28ba4c3c4145a5cbcd2ac8f4606a2bbe306
Lsapee <dlworhks94@gmail.com>
2022-02-18 23:47:21 +0900

    1~N까지 숫자 출력

---
+++ src/bronze/Nn.java
@@ -0,0 +1,15 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class Nn {
+    public static void main(String[] aa){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for(int i =1; i<=n; i++){
+            System.out.println(i);
+        }
+    }
+}
